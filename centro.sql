526ed0f1730182b8fb374d9a6d1dfb8d7b64cea4
Luis <luis.torresmo@alum.uca.es>
2022-03-21 19:43:17 +0100

    Are we gay ben?

--- centro.sql
+++ centro.sql
@@ -1,31 +1,31 @@
-CREATE TABLE centro (
+CREATE TABLE Centro (
     id_centro int(5) unsigned NOT NULL auto_increment,
     nombre_centro varchar(50) NOT NULL,
     localidad varchar(50) NOT NULL,
     PRIMARY KEY (id_centro)
 );
 
-INSERT INTO centro VALUES(
+INSERT INTO Centro VALUES(
     1, 
     'Escuela Superior de Ingeniería',
     'Puerto Real'
 );
-INSERT INTO centro VALUES(
+INSERT INTO Centro VALUES(
     2, 
     'Facultad de Derecho',
     'Jerez de la Frontera'
 );
-INSERT INTO centro VALUES(
+INSERT INTO Centro VALUES(
     3, 
     'Facultad de Medicina',
     'Cádiz'
 );
-INSERT INTO centro VALUES(
+INSERT INTO Centro VALUES(
     4, 
     'Escuela de Arte',
     'Jerez de La Frontera'
 );
-INSERT INTO centro VALUES(
+INSERT INTO Centro VALUES(
     5, 
     'CASEM',
     'Puerto Real'
