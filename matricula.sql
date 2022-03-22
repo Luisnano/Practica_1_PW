85fe0d96ab555aac4f06e8a41932d7637bdb1c76
Luis <luis.torresmo@alum.uca.es>
2022-03-22 17:09:46 +0100

    venga omeeee

---
+++ matricula.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Matricula (
+    id_asignatura int(5), 
+    id_estudiante int(5),
+    nota_final float(5),
+    PRIMARY KEY (id_asignatura, id_estudiante),
+    FOREIGN KEY id_asignatura REFERENCES Asignatura (id_asignatura),
+    FOREIGN KEY id_estudiante REFERENCES Estudiante (id_estudiante)
+);
+INSERT INTO  Matricula VALUES(
+    2,
+    1, 
+    4.98
+);
+INSERT INTO Matricula VALUES(
+    1,
+    1,
+    10
+);
+
+
+
+
+
+
+
+
+
