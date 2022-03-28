ccc718e551bbdbeda3fee468bd96d506f1cc289f
Luis <luis.torresmo@alum.uca.es>
2022-03-28 16:52:49 +0200

    Merge branch 'main' of https://github.com/Luisnano/Practica_1_PW

--- examen.sql
+++ examen.sql
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-CREATE TABLE Examen (
-    id_examen int(5) unsigned NOT NULL auto_increment,
-    id_alumno int(5) unsigned NOT NULL, 
-    id_pregunta varchar(200) NOT NULL, 
-    nota_examen int(5),
-    PRIMARY KEY (id_examen),
-    FOREIGN KEY (id_alumno) REFERENCES Grado (id_alumno)
-);
-
-INSERT INTO Examen VALUES (
-    1,
-    1,
-    (1, 2),
-    5
-=======
 CREATE TABLE Examen (
     id_examen int(5) unsigned NOT NULL auto_increment,
     id_alumno int(5), 
@@ -28,5 +12,4 @@ INSERT INTO Examen VALUES (
     1,
     (1, 2),
     5
->>>>>>> main
 );
