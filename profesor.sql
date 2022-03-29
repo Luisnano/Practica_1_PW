2c21b0cf2eac841b9094306dc2a0027cda93df9f
Luis <luis.torresmo@alum.uca.es>
2022-03-29 11:42:09 +0200

    changes

--- profesor.sql
+++ profesor.sql
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-CREATE TABLE Profesor (
-    id_profesor int(5) unsigned NOT NULL auto_increment,
-    id_asignatura int(5) unsigned NOT NULL, 
-    es_coordinador bit NOT NULL,
-    nombre_profesor varchar(50) NOT NULL,
-    PRIMARY KEY (id_profesor),
-);
-INSERT INTO Profesor VALUES(
-    1,
-    2, 
-    1,
-    'Jose Alonso de La Huerta'
-);
-INSERT INTO Profesor VALUES(
-    2,
-    1, 
-    1,
-    'Eneas Sanchez DonPerignon'
-);
-=======
 CREATE TABLE Profesor (
     id_profesor int(5) unsigned NOT NULL auto_increment,
     id_asignatura int(5), 
@@ -38,4 +17,3 @@ INSERT INTO Profesor VALUES(
     1,
     'Eneas Sanchez DonPerignon'
 );
->>>>>>> main
