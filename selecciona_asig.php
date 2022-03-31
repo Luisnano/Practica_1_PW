16f56b651dcd40258a1c83e0c15848a051e6f3b8
ara <aradxc56@gmail.com>
2022-03-31 15:52:23 +0200

    Arreglo seleccionar asig

--- selecciona_asig.php
+++ selecciona_asig.php
@@ -1,4 +1,6 @@
 <?php
+    session_start();
+    //print_r($_SESSION);
     include('assets/headers/header_alum.php');
 ?>
 <br>
@@ -11,13 +13,23 @@
         <?php
             include('config.php');
             $query_asignatura = $connection->query("SELECT * FROM asignatura");
-            //Hay que mostrar solos las asignaturas que esten verificados los alumnos
+            
             
             //Creacion del desplegable
             echo "<h2> Selecciona la asignatura de la que deseas hacer el examen: </h2>";
             echo "<select name='asignatura'>";
-                foreach($connection->query("SELECT * FROM asignatura") as $i){
-                    echo "<option value=".$i['id_asignatura'].">".$i['nombre']."</option>";
+                //Hay que mostrar solos las asignaturas que esten verificados los alumnos
+                foreach($connection->query("SELECT id_estudiante,id_asignatura from matricula") as $m){
+                    
+                    if($m['id_estudiante'] == $_SESSION['id']){
+                        $as_coincidente = $m['id_asignatura'];
+                        foreach($connection->query("SELECT * FROM asignatura WHERE id_asignatura LIKE $as_coincidente ") as $i){
+                            
+                            echo "<option value=".$i['id_asignatura'].">".$i['nombre']."</option>";
+                        
+                        }
+                    
+                    }
                 }
             echo "</select>";
 
