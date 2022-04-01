069bbe27fd994b9ea009bd7b935e77fd73b2d760
Luis <luis.torresmo@alum.uca.es>
2022-04-01 12:00:38 +0200

    Cambios

--- sql/matricula.sql
+++ sql/matricula.sql
@@ -3,8 +3,8 @@ CREATE TABLE Matricula (
     id_estudiante int(5) unsigned NOT NULL,
     nota_final float(5) unsigned NOT NULL,
     PRIMARY KEY (id_asignatura, id_estudiante),
-    FOREIGN KEY id_asignatura REFERENCES Asignatura (id_asignatura),
-    FOREIGN KEY id_estudiante REFERENCES Estudiante (id_estudiante)
+    FOREIGN KEY (id_asignatura) REFERENCES Asignatura (id_asignatura),
+    FOREIGN KEY (id_estudiante) REFERENCES Estudiante (id_estudiante)
 );
 INSERT INTO  Matricula VALUES(
     2,
