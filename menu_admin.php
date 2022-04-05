0a091b26bf1abbfb63b948c11f559a672948be08
Raúl Arcos Herrera <raularcosherrera@gmail.com>
2022-04-05 13:13:28 +0200

    otro jejeje

--- menu_admin.php
+++ menu_admin.php
@@ -7,14 +7,14 @@ include('assets/headers/header_admin.php');
     <div class="row">
       <div class="col-md-6">
         <div class="icon-box">
-          <i class="bi bi-pencil-square"></i>
+          <i class="bi bi-people-fill"></i>
           <h4><a href="listado_estudiantes.php">Gestión de Estudiantes</a></h4>
           <p>Añada, Edite o Borre Estudiantes de la BD.</p>
         </div>
       </div>
       <div class="col-md-6 mt-4 mt-md-0">
         <div class="icon-box">
-          <i class="bi bi-card-checklist"></i>
+          <i class="bi bi-person-fill"></i>
           <h4><a href="listado_prof.php">Gestión de Profesores</a></h4>
           <p>Añada, Edite o Borre Profesores de la BD.</p>
         </div>
